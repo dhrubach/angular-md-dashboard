b8dda5625a1308b52f6a7b4d5d29dbc9dee6e28d
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-06-05 23:04:10 +0530

    feat(app) : include sidenav component

---
+++ src/components/sidenav/sidenav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'adm-sidenav',
+	styles: [require('./sidenav.component.scss')],
+	template: require('./sidenav.template.html'),
+})
+export class SideNavComponent { }
--- src/main.module.ts
+++ src/main.module.ts
@@ -2,24 +2,42 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
 	MdButtonModule,
+	MdIconModule,
+	MdListModule,
 	MdSidenavModule,
 	MdToolbarModule,
 } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Route, RouterModule } from '@angular/router';
 
-import { DashboardAppComponent } from './components';
+import {
+	DashboardAppComponent,
+	SideNavComponent,
+} from './components';
+
+const routes: Route[] = [
+	{ path: '', component: DashboardAppComponent},
+];
 
 @NgModule({
 	bootstrap: [DashboardAppComponent],
-	declarations: [DashboardAppComponent],
+	declarations: [
+		DashboardAppComponent,
+		SideNavComponent,
+	],
 	imports: [
 		BrowserAnimationsModule,
 		BrowserModule,
 		FormsModule,
 		MdButtonModule,
+		MdIconModule,
+		MdListModule,
 		MdSidenavModule,
 		MdToolbarModule,
+		RouterModule.forRoot(routes, {
+			useHash: true,
+		}),
 	],
 	providers: [],
 })
