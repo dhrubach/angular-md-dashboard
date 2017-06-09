662e9d5ae6fab4dcd20c72564fcb11096460ada5
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-06-09 07:28:57 +0530

    feat(dashboard) : initial card layout

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'adm-dashboard',
+	styles: [require('./dashboard.component.scss')],
 	template: require('./dashboard.template.html'),
 })
 export class DashboardComponent { }
--- src/main.module.ts
+++ src/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
 	MdButtonModule,
+	MdCardModule,
 	MdIconModule,
 	MdListModule,
 	MdSidenavModule,
@@ -36,6 +37,7 @@ import { AppRoutingModule } from './main-routing.module';
 		BrowserModule,
 		FormsModule,
 		MdButtonModule,
+		MdCardModule,
 		MdIconModule,
 		MdListModule,
 		MdSidenavModule,
