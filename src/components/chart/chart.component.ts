a2abfcc53f63383249ae971dc3e858b0d5f40a84
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-06-20 19:48:54 +0530

    update(chart) : line chart components with animation

--- src/components/chart/chart.component.ts
+++ src/components/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { ChartType } from 'ng-chartist';
+import { ChartEvent, ChartType } from 'ng-chartist';
 
 import * as Chartist from 'chartist';
 
@@ -11,15 +11,39 @@ import * as Chartist from 'chartist';
 })
 export class ChartComponent {
 	private type: ChartType;
-	private data: Chartist.IChartistData;
+	private data: Chartist.IChartistData[];
+	private event: ChartEvent;
 
 	constructor() {
 		this.type = 'Line';
-		this.data = {
-			labels: [ 'M', 'T', 'W', 'T', 'F', 'S', 'S'],
-			series: [
-				[ 10, 50, 40, 55, 100, 30, 10],
-			],
+		this.data = [
+			{
+				labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+				series: [
+					[10, 50, 40, 55, 100, 30, 10],
+				],
+			},
+			{
+				labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+				series: [
+					[50, 90, 40, 55, 100, 30, 40],
+				],
+			},
+		];
+		this.event = {
+			draw: (data) => {
+				if (data.type === 'line' || data.type === 'area') {
+					data.element.animate({
+						d: {
+							begin: 600,
+							dur: 700,
+							easing: Chartist.Svg.Easing.easeOutQuint,
+							from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+							to: data.path.clone().stringify(),
+						},
+					});
+				}
+			},
 		};
 	}
 }
