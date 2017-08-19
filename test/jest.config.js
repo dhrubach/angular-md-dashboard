8dca821b8f2d5b118aa28aaab261c0557ba74da5
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-08-19 22:10:22 +0530

    chore(test) : move jest configuration to ./test

---
+++ test/jest.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+	"collectCoverage": true,
+	"collectCoverageFrom": ["**/*.ts", "!**/test/**"],
+	"coverageDirectory": "<rootDir>/test/coverage/jest",
+	"globals": {
+		"Chartist": {},
+		"ts-jest": {
+			"tsConfigFile": "tsconfig.spec.json",
+			"useBabelrc": true
+		},
+		"__TRANSFORM_HTML__": true,
+	},
+	"mapCoverage": true,
+	"moduleDirectories": [
+		"node_modules",
+		"src"
+	],
+	"moduleFileExtensions": [
+		"ts",
+		"js",
+		"html",
+		"json"
+	],
+	"moduleNameMapper": {
+		"^.+\\.*(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/assetsTransformer.js"
+	},
+	"rootDir": "../",
+	"setupTestFrameworkScriptFile": "<rootDir>/test/setup-jest.js",
+	"testRegex": "(/__tests__/.*|\\.spec)\\.(ts|js)$",
+	"transform": {
+		"^.+\\.(ts|js|html)$": "<rootDir>/test/preprocessor.js"
+	},
+	"transformIgnorePatterns": [
+		"node_modules/(?!@ngrx)"
+	],
+	"verbose": true,
+	"watchman": false,
+};
